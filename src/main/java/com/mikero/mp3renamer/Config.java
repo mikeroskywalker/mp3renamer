10ee562e91ab708a83fe4f2df53bf4c0bafff8a6
MikeroSkywalker <puzhwei@gmail.com>
2016-07-17 01:58:42 -0400

    add config java

---
+++ src/main/java/com/mikero/mp3renamer/Config.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mikero.mp3renamer;
+
+/**
+ * @author peter
+ *
+ */
+public class Config {
+	private Path paths;
+
+	public Path getPath() {
+		return paths;
+	}
+
+	public void setPath(Path paths) {
+		this.paths = paths;
+	}
+}
