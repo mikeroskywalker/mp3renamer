7c16802615bc0ab135b33a11bf3945109e8a249a
MikeroSkywalker <puzhwei@gmail.com>
2016-07-19 00:09:41 -0400

    add path

--- src/main/java/com/mikero/mp3renamer/Path.java
+++ src/main/java/com/mikero/mp3renamer/Path.java
@@ -9,5 +9,18 @@ package com.mikero.mp3renamer;
  */
 public class Path {
 	private String sourcePath;
-  private String targetPath;
+	private String targetPath;
+
+	public String getSorucePath() {
+		return sourcePath;
+	}
+	public void setSorucePath(String sourcePath) {
+		this.sourcePath = sourcePath;
+	}
+	public String getTargetPath() {
+		return targetPath;
+	}
+	public void setTargetPath(String targetPath) {
+		this.targetPath = targetPath;
+	}
 }
